cc86b78ad403f9a969ee29ab42463d13439ce61b
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-03 04:05:49 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -38,10 +38,8 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest["muskets"]= "2"
-  shipping_manifest["gun powder"]= "4"
-  puts shipping_manifest
 end
-
-  
+shipping_manifest["muskets"]= "2"
+shipping_manifest["gun powder"]= "4"
   puts shipping_manifest
+  
