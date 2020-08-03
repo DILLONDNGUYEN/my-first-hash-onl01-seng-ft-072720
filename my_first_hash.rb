73048b16e706a1592a79dfdc0c0a209512634366
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-03 00:10:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,7 +30,7 @@ end
 
 
 # add 2 muskets to the shipping_manifest hash below
-
+# add 4 gun powder to the shipping_manifest hash below
 def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
@@ -42,7 +42,7 @@ def adding
 
 
 
-  # add 4 gun powder to the shipping_manifest hash below
+  
 
 
 
